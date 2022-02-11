7951a1f30786bd6f171d3632ad04967df371fb71
metadaoagents <98770288+metadaoagents@users.noreply.github.com>
2022-02-11 19:23:03 +0000

    Update DappToken.sol

--- src/contracts/DappToken.sol
+++ src/contracts/DappToken.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract RewardToken is ERC20 {
+contract DappToken is ERC20 {
     constructor() public ERC20("Reward Token", "RWT") {
         _mint(msg.sender, 1000000000000000000000000);
     }
